6f7cf031c164f81668216ac1405770277446dfc8
ispeedonthe405 <ispeedonthe405@gmail.com>
2024-06-28 19:37:37 -0700

    UX changes, paving the way for account and mailbox-group switching

--- ux/MainWindow.xaml.cs
+++ ux/MainWindow.xaml.cs
@@ -8,6 +8,20 @@ namespace GroovyMail.ux
         public MainWindow()
         {
             this.InitializeComponent();
+            PaneHeader.cbxMailbox.SelectionChanged += CbxMailbox_SelectionChanged;
+        }
+
+        private void CbxMailbox_SelectionChanged(object sender, Microsoft.UI.Xaml.Controls.SelectionChangedEventArgs e)
+        {
+            NavMain.MenuItems.Clear();
+            NavMain.MenuItems.Add(new Microsoft.UI.Xaml.Controls.NavigationViewItem() { Content = $"Inbox{PaneHeader.cbxMailbox.SelectedIndex}" });
+        }
+
+        private void NavMain_SelectionChanged(
+            Microsoft.UI.Xaml.Controls.NavigationView sender, 
+            Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+
         }
     }
 }
